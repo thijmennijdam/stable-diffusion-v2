b9ad6e049244e3b3811167e89898484c666d901f
Thijmen Nijdam <thijmen.nijdam@gmail.com>
2025-05-14 12:03:25 +0200

    update alpha list

--- jobs/run_different_alpha_jobs.sh
+++ jobs/run_different_alpha_jobs.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="${SCRIPT_DIR}/.."
 
 # List of alpha values
-ALPHAS=(0 0.05 0.1 0.2 0.4 0.6 0.8 1.0)
+ALPHAS=(0 0.01 0.05 0.1 0.15 0.2)
 
 # Inference arguments
 PROMPT="a photo of a cat"
